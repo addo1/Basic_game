39aaeaeeea68b39e4af0dd4aa8cc7031f0447ca5
xsouku17 <xsouku17@stud.fit.vutbr.cz>
2024-05-22 17:17:42 +0200

    first version of enemy in base game

--- scripts/npc/base_npc.gd
+++ scripts/npc/base_npc.gd
@@ -13,10 +13,12 @@ extends CharacterBody3D
 var chased_target = null
 
 var is_dead = false
+var start_movement = false
 
 func _ready():
 	if has_node("Health"):
 		get_node("Health").died.connect(_on_died)
+	$monster/AnimationPlayer.play("block")
 
 	nav_agent.velocity_computed.connect(_on_velocity_computed)
 	nav_agent.navigation_finished.connect(_on_navigation_finished)
@@ -35,7 +37,25 @@ func _ready():
 		area.add_child(collision)
 		collision.shape = SphereShape3D.new()
 		collision.shape.radius = aggro_range
-
+	var collision_area = Area3D.new()
+	var collision_shape = CollisionShape3D.new()
+	add_child(collision_area)
+	collision_area.add_child(collision_shape)
+	collision_shape.shape = BoxShape3D.new()
+	collision_shape.shape.extents = Vector3(2, 2, 2)
+	collision_area.body_entered.connect(_on_body_entered)
+	collision_area.collision_layer = 0
+	collision_area.collision_mask = 16
+	
+func _on_body_entered(body):
+	if body is CharacterBody3D:
+		print("Nepřítel narazil do hráče!")
+		start_movement = false
+		$monster/AnimationPlayer.play("attacking")
+		$monster/AnimationPlayer.animation_finished.connect(_on_starting_animation_finished)
+		#var health = body.get_node("Health") if body.has_node("Health") else null
+		#if health:
+			#health.take_damage(10)
 
 func _init_navigation():
 	if is_patrolling:
@@ -45,7 +65,7 @@ func _physics_process(_delta):
 	if is_dead:
 		return
 
-	if chased_target != null:
+	if chased_target != null and start_movement:
 		_set_navigation_target(chased_target.global_position)
 	_process_movement()
 
@@ -62,12 +82,17 @@ func _choose_next_patrol_point_index(current_point_index, patrol_point_num):
 		return current_point_index + 1
 
 func _process_movement():
+	if not start_movement:
+		return
 	var next_path_position: Vector3 = nav_agent.get_next_path_position()
 	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * movement_speed
 	if nav_agent.avoidance_enabled:
 		nav_agent.set_velocity(new_velocity)
 	else:
 		_on_velocity_computed(new_velocity)
+		
+	if new_velocity.length() > 0.01:
+		look_at(global_position + new_velocity, Vector3.UP)
 
 func _on_navigation_finished():
 	if is_patrolling:
@@ -81,6 +106,14 @@ func _on_aggro_range_entered(body):
 	if body is CharacterBody3D:
 		chased_target = body
 		_set_navigation_target(chased_target.global_position)
+		$monster/AnimationPlayer.play("starting")
+		$monster/AnimationPlayer.animation_finished.connect(_on_starting_animation_finished)
+		
+func _on_starting_animation_finished(anim_name):
+	if anim_name == "starting" or "attacking":
+		$monster/AnimationPlayer.play("walking")
+		$monster/AnimationPlayer.animation_finished.disconnect(_on_starting_animation_finished)
+		start_movement = true
 
 func launch_character(launch_velocity, _allow_air_control = true):
 	velocity = launch_velocity
@@ -92,3 +125,4 @@ func _on_died():
 		damage_area.set_deferred("monitoring", false)
 		damage_area.set_deferred("monitorable", false)
 		velocity = Vector3.ZERO
+		$monster/AnimationPlayer.play("dying")
