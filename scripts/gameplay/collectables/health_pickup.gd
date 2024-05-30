3dd72ac62a349a5e1f6b5f6590536b0db9d42943
addo1 <e7lt@hotmail.com>
2024-05-30 21:44:18 +0200

    +collectible

--- scripts/gameplay/collectables/health_pickup.gd
+++ scripts/gameplay/collectables/health_pickup.gd
@@ -4,25 +4,25 @@ extends Area3D
 @export var heal_amount = 10
 
 func _ready():
-    # connect the body_entered signal to the _on_pickup_body_entered function when starting play
-    body_entered.connect(_on_pickup_body_entered)
+	# connect the body_entered signal to the _on_pickup_body_entered function when starting play
+	body_entered.connect(_on_pickup_body_entered)
 
 func _on_pickup_body_entered(body):
-    # check if the body entered has the Health node
-    # if it doesn't, do nothing
-    if body.has_node("Health"):
-        var health_node = body.get_node("Health")
-        if health_node != null:
-            health_node.heal(heal_amount)
-            # disable the health trigger
-            set_deferred("monitoring", false)
-            set_deferred("monitorable", false)
-            # hide the visual
-            visible = false
-            # play the health sound and wait for it to finish
-            $HealthSound.play()
-            $HealthSound.finished.connect(_on_health_sound_finished)
+	# check if the body entered has the Health node
+	# if it doesn't, do nothing
+	if body.has_node("Health"):
+		var health_node = body.get_node("Health")
+		if health_node != null:
+			health_node.heal(heal_amount)
+			# disable the health trigger
+			set_deferred("monitoring", false)
+			set_deferred("monitorable", false)
+			# hide the visual
+			visible = false
+			# play the health sound and wait for it to finish
+			$HealthSound.play()
+			$HealthSound.finished.connect(_on_health_sound_finished)
 
 func _on_health_sound_finished():
-    # destroy the pickup when sound is finished
-    queue_free()
+	# destroy the pickup when sound is finished
+	queue_free()
--- scripts/gameplay/platforming/spring.gd
+++ scripts/gameplay/platforming/spring.gd
@@ -4,16 +4,16 @@ extends Node3D
 @export var spring_velocity = 10
 
 func _trigger_area_body_entered(body):
-    if body is PlayerCharacter:
-        # if the body is a player, wait for them to land on the spring
-        body.landed.connect(Callable(self, "_on_player_landed").bindv([body]))
+	if body is PlayerCharacter:
+		# if the body is a player, wait for them to land on the spring
+		body.landed.connect(Callable(self, "_on_player_landed").bindv([body]))
 
 func _trigger_area_body_exited(body):
-    if body is PlayerCharacter:
-        # if the body is a player, disconnect the landed signal
-        body.landed.disconnect(Callable(self, "_on_player_landed").bindv([body]))
+	if body is PlayerCharacter:
+		# if the body is a player, disconnect the landed signal
+		body.landed.disconnect(Callable(self, "_on_player_landed").bindv([body]))
 
 func _on_player_landed(body):
-    # launch the player when they've landed
-    body.velocity.y = spring_velocity
-    $SpringSound.play()
+	# launch the player when they've landed
+	body.velocity.y = spring_velocity
+	$SpringSound.play()
