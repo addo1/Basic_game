7d2993eb0a3966635c1e69cd3a9bdb72949f7de5
xsouku17 <xsouku17@stud.fit.vutbr.cz>
2024-06-10 13:05:23 +0200

    edit of main menu

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -8,13 +8,6 @@ func start_fps_level():
 	var scene = load("res://scenes/example_levels/fps_level.tscn")
 	get_parent().load_scene_and_set_current(scene)
 
-func start_third_person_level():
-	var scene = load("res://scenes/example_levels/third_person_level.tscn")
-	get_parent().load_scene_and_set_current(scene)
-
-func start_platformer_level():
-	var scene = load("res://scenes/example_levels/platformer_level.tscn")
-	get_parent().load_scene_and_set_current(scene)
 
 func exit_game():
 	get_tree().quit()
