0f1b7aebf9f20a022dbe438ccbacc7be1c5084f5
EliasPonti <lukus.el@gmail.com>
2024-06-11 11:41:54 +0200

    first text made

--- scripts/gameplay/bison.gd
+++ scripts/gameplay/bison.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 @export var required_soup = 3
 @export var was_transformed = false
+@onready var bison_text = $BisonText
+@export var text_counter = 0
 
 
 func _ready():
@@ -9,6 +11,7 @@ func _ready():
 
 func _on_bison_area_body_entered(body):
 	if body.has_node("CoinStash"):
+		_text_on_bison_enter()
 		var coin_stash_node = body.get_node("CoinStash")
 		if required_soup == coin_stash_node.coins_in_stash:
 			var ammo = get_node("/root/Game/FPSLevel/AmmoPickup")
@@ -20,3 +23,11 @@ func _on_bison_area_body_entered(body):
 				was_transformed = true
 		else:
 			print("not enough soup")
+			
+func _text_on_bison_enter():
+	match text_counter:
+		1:
+			bison_text.visible = true
+			await get_tree().create_timer(10.0).timeout
+			bison_text.visible = false
+	
