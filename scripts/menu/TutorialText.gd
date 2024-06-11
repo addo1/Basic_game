5391f4e5a33afc3ccb7e884a916c14e2e309c402
EliasPonti <lukus.el@gmail.com>
2024-06-11 14:18:47 +0200

    tutorial text change

--- scripts/menu/TutorialText.gd
+++ scripts/menu/TutorialText.gd
@@ -4,6 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	await get_tree().create_timer(0.2).timeout
 	text.visible = true
 	await get_tree().create_timer(12.0).timeout
 	text.visible = false
